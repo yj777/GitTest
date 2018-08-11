403bb84b2a8bc251de24534e8c140fa1efb40635
yj777 <41776257+yj777@users.noreply.github.com>
2018-08-11 11:44:59 +0800

    调用compare

--- main.cpp
+++ main.cpp
@@ -1,2 +1,15 @@
 #include<iostream>
 using namespace std;
+
+//主函数
+int main()
+{
+  cout<<"hello word!"<<end1;
+  compare(1,2); 
+  return 0;
+}
+
+tool compare(int a,int b)
+{
+  return a>b?true:false;
+}
